452de98fca0bad20a8485811f94bcbbd742e362a
MichaelHoppe <Michael.Hoppe@fiz-karlsruhe.de>
2012-02-08 09:23:01 +0000

    add content-type and char-encoding headers
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/branches/SRW/1.3/SRW-Repository/@45373 5c398c58-3d0a-0410-a38c-d0825f6816e0

--- src/java/ORG/oclc/os/SRW/SRWServlet.java
+++ src/java/ORG/oclc/os/SRW/SRWServlet.java
@@ -1440,6 +1440,8 @@ public class SRWServlet extends AxisServlet {
 			if (response != null) {
 				ServletOutputStream out = null;
 				try {
+					resp.setContentType("text/xml");
+					resp.setCharacterEncoding("UTF-8");
 					out = resp.getOutputStream();
 					IOUtils.copyAndCloseInput(response.getInputStream(), out);
 				} catch (Exception e) {
@@ -1540,6 +1542,7 @@ public class SRWServlet extends AxisServlet {
             Message respMsg=msgContext.getResponseMessage();
             if(respMsg!=null) {
                 resp.setContentType("text/xml");
+				resp.setCharacterEncoding("UTF-8");
                 PrintWriter writer=resp.getWriter();
                 // code to strip SOAP stuff out.  Hope this can go away some day
                 String soapResponse=respMsg.getSOAPPartAsString();
